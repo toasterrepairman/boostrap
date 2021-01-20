7f8587b90a417521e076d31dd9d5a6b71e574dba
toast <joelbasar@gmail.com>
2021-01-19 21:30:14 -0500

    Basic setup (non-functional)

---
+++ boostrap.sh
@@ -0,0 +1,20 @@
+# boostrap, a nordic Arch bootstrap
+
+echo "-- boostrap --"
+echo "the following script will install a lot of dotfiles you may not want"
+
+sleep 2
+
+echo "exit now if you are not sure of what you are doing"
+
+sleep 5
+
+sudo pacman -Syu
+# system upgrade
+sudo pacman -S fish
+# installing essentials (lightweight only)
+sudo chsh -S /bin/fish
+#setting fish to default shell
+
+
+# i stay norded // i've seen footage
