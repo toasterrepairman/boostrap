312d94b3f5e3ad153a9851fed441309215609372
toasterrepairman <smol@toast.cyou>
2021-05-31 13:01:29 -0400

    added rudimentary dotfile installation

--- boostrap.sh
+++ boostrap.sh
@@ -9,14 +9,24 @@ echo "exit now if you are not sure of what you are doing"
 
 sleep 5
 
-sudo pacman -Syu
 # system upgrade
-sudo pacman -S fish yay micro
-# installing essentials (lightweight only)
-sudo chsh -S /bin/fish
-# setting fish to default shell
+sudo pacman -Syu
 
-sudo yay -S ciano kdenlive qalculate spotify glade ghostwriter discord
+# installing essentials (lightweight only)
+sudo pacman -S fish yay micro
+
+# setting fish to default shell
+sudo chsh -S /bin/fish
+
 # installing bonus programs
+sudo yay -S curlew kdenlive qalculate spotify glade ghostwriter discord albert
+
+# bootstrapping GTK theme
+mkdir ~/.themes/ && cp resouces/Pop-nord-dark.zip ~/.themes/ 
+gsettings set org.gnome.shell.extensions.user-theme "Pop-nord-dark"
+gsettings set org.gnome.desktop.interface gtk-theme "Pop-nord-dark"
+
+# restoring config
+cp -r resources/config/* ~/.config/
 
 # i stay norded // i've seen footage
