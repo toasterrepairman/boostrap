18dc94aaab951e50c334f46d0d55c04aa1b1ad25
toasterrepairman <smol@toast.cyou>
2021-07-02 15:01:07 -0400

    added more configs

---
+++ resources/scripts/distfixer.sh
@@ -0,0 +1,8 @@
+# run this inside a proton folder (the one you're using for bf4)
+
+sudo setcap cap_net_raw+epi dist/bin/wineserver
+
+sudo setcap cap_net_raw+epi dist/bin/wine-preloader
+
+sudo setcap cap_net_raw+epi dist/bin/wine64-preloader
+
