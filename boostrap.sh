a28984744e87f15b2f82dfbac053068d0231d0e1
toast <smol@toast.cyou>
2021-07-08 15:05:13 -0400

    fixed CSS ricing

--- boostrap.sh
+++ boostrap.sh
@@ -73,8 +73,14 @@ cd ..
 
 # installing spicetify
 spicetify backup apply
+spicetify config custom_apps lyrics-plus
+spicetify config custom_apps new-releases
+spicetify config extensions bookmark.js
+spicetify config extensions loopyLoop.js 
+spicetify config extensions shuffle+.js
 spicetify config color_scheme nord-dark
 spicetify apply
+
 
 # installing retroarch/libretro cores
 yay -S retroarch retroarch-assets-ozone libretro-beetle-pce libretro-beetle-psx libretro-beetle-supergrafx libretro-blastem libretro-bsnes libretro-citra libretro-desmume libretro-dolphin libretro-gambatte libretro-mgba libretro-mupen64plus-next
