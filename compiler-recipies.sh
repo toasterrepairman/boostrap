51325eb1ad7d04c9fc863ae05adf13fd53e72257
toast <smol@toast.cyou>
2021-09-28 20:32:41 -0400

    dunk, working build

--- compiler-recipies.sh
+++ compiler-recipies.sh
@@ -1,5 +1,10 @@
+# projectpad build hell (best guess)
+yay -S luarocks sqlcipher
+
+sudo luarocks install lsqlcipher
+
 git clone https://github.com/emmanueltouzery/projectpad2.git
 
-cd projectpad2
+cd projectpad2/projectpad
 
 cargo build --release
