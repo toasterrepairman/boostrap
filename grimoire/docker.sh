5f60df5f64ee4973ddeca5ccaacd4ad84d478965
toast <smol@toast.cyou>
2022-03-07 08:30:08 -0500

    start Docker service in systemd

--- grimoire/docker.sh
+++ grimoire/docker.sh
@@ -3,7 +3,7 @@ sudo pacman -S docker
 sudo tee /etc/modules-load.d/loop.conf <<< "loop"
 modprobe loop
 
-sudo systemctl enable --now snapd.socket
-
 sudo groupadd docker
 sudo usermod -aG docker toast
+
+systemctl start docker.service
