8c3677d91939400315bf4bf8f909ea4eee87493d
toast <smol@toast.cyou>
2022-03-07 08:29:09 -0500

    added loop module

--- grimoire/docker.sh
+++ grimoire/docker.sh
@@ -1,2 +1,9 @@
+sudo pacman -S docker
+
+sudo tee /etc/modules-load.d/loop.conf <<< "loop"
+modprobe loop
+
+sudo systemctl enable --now snapd.socket
+
 sudo groupadd docker
 sudo usermod -aG docker toast
