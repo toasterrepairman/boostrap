a0e41f260032289d1cdf001ef0d0ade67c6fdfb1
toast <smol@toast.cyou>
2022-03-10 08:56:49 -0500

    added personal file remocer

--- grimoire/doormat.sh
+++ grimoire/doormat.sh
@@ -6,9 +6,23 @@ echo "clearing SSH credentials..."
 rm ~/.ssh/*
 
 # delete all homebrew packages
-echo "removing all brew packages"
+echo "removing all brew packages..."
 brew remove $(brew list --formulae) $(brew list --cask) 
 
 # clean config directory
-echo "deleting config directory"
+echo "deleting config directory..."
 rm -r ~/.config/*
+
+# remove all modified directories
+echo "clearing personal directories..."
+echo "---"
+echo "this command will delete a lot of files from ~/Documents/ and other spots"
+echo "some of these files may contain personal data or uncommit changes"
+
+# confirm deletion via interactive block
+read -p "continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) rm -rf ~/Documents/* ~/Downloads/* ~/Music/* ~/Pictures/*;;
+  n|N ) echo "skipping this step...";;
+  * ) echo "invalid, skipping step";;
+esac
