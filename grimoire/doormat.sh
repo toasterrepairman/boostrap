0c2f11ee8b8054d6c755840d33790e11477e652f
toast <smol@toast.cyou>
2022-03-09 19:02:17 -0500

    added doormat

---
+++ grimoire/doormat.sh
@@ -0,0 +1,10 @@
+# doormat
+# a place to wipe your bootstrap
+
+# removes creds
+echo "clearing SSH credentials..."
+rm ~/.ssh/*
+
+# delete all homebrew packages
+echo "removing all brew packages"
+brew remove $(brew list --formulae) $(brew list --cask) 
