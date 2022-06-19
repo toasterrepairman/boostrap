5393ded9f77a7007bc6977ec63db4a89b7ae9b62
Joel <smol@toast.cyou>
2022-06-18 23:43:07 -0400

    Overhauled Git setup

--- nix/nixpkgs/home.nix
+++ nix/nixpkgs/home.nix
@@ -21,6 +21,14 @@
     };
   };
 
+  programs.git = {
+    enable = true;
+    userName  = "toasterrepairman";
+    userEmail = "fixing@toast.cyou";
+  };
+  
+  programs.gitui.enable = true; 
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
