f6214365bf97850417f8fe2dc41a4b5b631da5c3
Joel B <smol@toast.cyou>
2023-01-13 10:09:41 -0500

    changed graphviz script

--- grimoire/systemd-graphviz.sh
+++ grimoire/systemd-graphviz.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Use systemd-cgtop to get a list of running systemd processes
-processes=$(systemd-cgtop -bn1)
+# Use ps to get a list of running processes
+processes=$(ps -eo pid,ppid,comm)
 
 # Create a graphviz file
 echo "digraph {" > processes.dot
 
 # Loop through the processes and add edges to the graphviz file
 while read -r line; do
-  process_name=$(echo $line | awk '{print $1}')
-  process_path=$(echo $line | awk '{print $2}')
-  echo "\"$process_name\" -> \"$process_path\";" >> processes.dot
+  parent_pid=$(echo $line | awk '{print $2}')
+  child_pid=$(echo $line | awk '{print $1}')
+  process_name=$(echo $line | awk '{print $3}')
+  echo "\"$parent_pid\" -> \"$child_pid\" [ label = \"$process_name\"];" >> processes.dot
 done <<< "$processes"
 
 echo "}" >> processes.dot
