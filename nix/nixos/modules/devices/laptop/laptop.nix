1c8318427feab1aefeb0fe3d8c1f14ba090440e4
Joel <smol@toast.cyou>
2023-04-16 01:02:50 -0400

    fingerprimpt?

--- nix/nixos/modules/devices/laptop/laptop.nix
+++ nix/nixos/modules/devices/laptop/laptop.nix
@@ -36,5 +36,6 @@
   hardware.bluetooth.enable = true;
 
   services.fprintd.enable = true;
-  # services.fprintd.tod.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
 }
