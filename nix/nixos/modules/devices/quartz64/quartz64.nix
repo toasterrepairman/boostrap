014cffeeac0b49e90c01ed917da28896b6f8ab56
Joel B <smol@toast.cyou>
2023-05-11 00:53:29 -0400

    kodi

--- nix/nixos/modules/devices/quartz64/quartz64.nix
+++ nix/nixos/modules/devices/quartz64/quartz64.nix
@@ -31,6 +31,12 @@
   #   enableSSHSupport = true;
   # };
 
+  
+  services.xserver.enable = true;
+  services.xserver.desktopManager.kodi.enable = true;
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kodi";
+
   users.extraUsers.kodi.isNormalUser = true;
   services.cage.user = "kodi";
   services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
