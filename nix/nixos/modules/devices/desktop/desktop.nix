79dcc999da63c3dc41e80b93aa95401e2e3c9701
Joel B <smol@toast.cyou>
2023-05-22 13:06:14 -0400

    hmm trying samba

--- nix/nixos/modules/devices/desktop/desktop.nix
+++ nix/nixos/modules/devices/desktop/desktop.nix
@@ -43,7 +43,45 @@
   #   toastvpn = { config = 
   #     '' config /home/toast/Documents/Cloud/Lifehub/Computer/VPN/openvpn-toast.key ''; 
   #    };
-  # };  
+  # };
+
+  services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+  networking.firewall.allowedTCPPorts = [
+    5357 # wsdd
+  ];
+  networking.firewall.allowedUDPPorts = [
+    3702 # wsdd
+  ];
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+      #workgroup = WORKGROUP
+      server string = toaster
+      netbios name = toaster
+      security = user
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.0. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      public = {
+        path = "/run/media/toast/Leviathan 1/Archives/Videos/";
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+        # "create mask" = "0644";
+        # "directory mask" = "0755";
+        # "force user" = "username";
+        # "force group" = "groupname";
+      };
+    };
+  };
+
 
   environment.systemPackages = with pkgs; [
     # davinci-resolve
