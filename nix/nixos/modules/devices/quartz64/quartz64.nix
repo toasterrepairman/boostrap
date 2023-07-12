917968208255e466a18f2fca4a11d10a6570b65b
Joel B <smol@toast.cyou>
2023-07-12 16:33:12 -0400

    opened ports

--- nix/nixos/modules/devices/quartz64/quartz64.nix
+++ nix/nixos/modules/devices/quartz64/quartz64.nix
@@ -76,7 +76,7 @@
     services.minidlna.openFirewall = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 80 8080 1234 8096 8090 3000 3001 3002 3003 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 8080 1234 2283 8096 8090 3000 3001 3002 3003 ];
   networking.firewall.allowedUDPPorts = [ 7359 1900 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
