4ef47175e2d9f3b328653119d396586523b196c1
Joel B. <smol@toast.cyou>
2023-12-27 16:23:32 -0500

    535 rollback cope

--- nix/nixos/modules/devices/desktop/desktop.nix
+++ nix/nixos/modules/devices/desktop/desktop.nix
@@ -6,13 +6,38 @@
 
   # Appease the Nvidia Gods
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = true;
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   hardware.pulseaudio.support32Bit = true;
   hardware.steam-hardware.enable = true;  
-  hardware.nvidia.powerManagement.enable = true;
-  hardware.nvidia.modesetting.enable = true;
   virtualisation.docker.enableNvidia = true;
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.enable = true;
+    # powerManagement.finegrained = true;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+  	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+  };
  
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
@@ -50,6 +75,7 @@
     # davinci-resolve
     xorg.libxcb
     cudaPackages.cudatoolkit
+    # linuxKernel.packages.linux_6_6.nvidia_x11_production
   ];
 
   hardware.opengl.driSupport32Bit = true;
