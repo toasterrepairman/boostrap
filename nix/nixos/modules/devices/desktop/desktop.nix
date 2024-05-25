ef0bd642dcffdccfa3834fa2735851c148fb88fc
Joel B. <smol@toast.cyou>
2024-05-25 13:15:24 -0400

    betterer drivers

--- nix/nixos/modules/devices/desktop/desktop.nix
+++ nix/nixos/modules/devices/desktop/desktop.nix
@@ -13,7 +13,7 @@
   nixpkgs.config.nvidia.acceptLicense = true;
 
   # boot.kernelPackages = pkgs.linuxPackages.extend (self: super: {
-  #   nvidia_x11 = super.nvidia_x11_dc_535;
+  #   nvidia_x11 = super.nvidia_x11_beta;
   # });
   
   services.xserver = {
@@ -47,15 +47,15 @@
     # package = config.boot.kernelPackages.nvidiaPackages.;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.production;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.42.02";
-      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-      sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
-      openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
-      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #   version = "555.42.02";
+    #   sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+    #   sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
+    #   openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
+    #   settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+    #   persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
+    # };
   };
       
  
