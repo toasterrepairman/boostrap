980bcfaf62991f92b07fb04eb9be30257942cb53
Joel B. <smol@toast.cyou>
2024-12-05 15:27:57 -0500

    desktop update

--- nix/nixos/modules/devices/desktop/desktop.nix
+++ nix/nixos/modules/devices/desktop/desktop.nix
@@ -107,7 +107,6 @@
   environment.systemPackages = with pkgs; [
     # davinci-resolve
     xorg.libxcb
-    alpaca
     # cudaPackages.cudatoolkit
     # linuxKernel.packages.linux_6_6.nvidia_x11_production
   ];
