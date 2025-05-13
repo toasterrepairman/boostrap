a8966c6bc2cbe1c7db678941a67c61106e3dfc2d
Joel B. <smol@toast.cyou>
2025-05-12 21:43:36 -0400

    updated test flake

--- grimoire/toys/opentrack/flake.nix
+++ grimoire/toys/opentrack/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Opentrack with ONNX support";
+  description = "Head tracking software with ONNX support";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,31 +9,89 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-        # Define aruco as a local dependency
-        aruco = pkgs.callPackage ./aruco.nix { };
+        opentrack = pkgs.qt5.mkDerivation rec {
+          pname = "opentrack";
+          version = "2023.3.0";
 
-        opentrackWithOnnx = pkgs.opentrack.overrideAttrs (oldAttrs: {
-          buildInputs = oldAttrs.buildInputs ++ [ pkgs.onnxruntime ];
+          src = pkgs.fetchFromGitHub {
+            owner = "opentrack";
+            repo = "opentrack";
+            rev = "opentrack-${version}";
+            hash = "sha256-C0jLS55DcLJh/e5yM8kLG7fhhKvBNllv5HkfCWRIfc4=";
+          };
 
-          cmakeFlags = oldAttrs.cmakeFlags ++ [
-            "-DONNXRuntime_DIR=${pkgs.onnxruntime}/lib/cmake/onnxruntime"
-            "-DONNXRuntime_INCLUDE_DIR=${pkgs.onnxruntime}/include/onnxruntime"
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+            ninja
+            cmake
+            copyDesktopItems
+            qt5.wrapQtAppsHook
+            onnxruntime.dev
           ];
-        });
+
+          env.NIX_CFLAGS_COMPILE = "-Wall -Wextra -Wpedantic -ffast-math -O3";
+          dontWrapQtApps = true;
+
+          buildInputs = with pkgs; [
+            qt5.qtbase
+            qt5.qttools
+            opencv4
+            procps
+            eigen
+            xorg.libXdmcp
+            libevdev
+            onnxruntime.dev
+          ] ++ lib.optionals stdenv.targetPlatform.isx86_64 [ wineWowPackages.stable ];
+
+          nativeCheckInputs = [
+            pkgs.onnxruntime
+          ];
+
+          cmakeFlags = [
+            "-GNinja"
+            "-DCMAKE_BUILD_TYPE=Release"
+            # Onnxruntime configuration
+            "-DONNXRuntime_DIR=${pkgs.onnxruntime}/lib/cmake/onnxruntime/"
+            "-DONNXRuntime_INCLUDE_DIR=${pkgs.onnxruntime}/include/"
+          ] ++ pkgs.lib.optionals pkgs.stdenv.targetPlatform.isx86_64 [ "-DSDK_WINE=ON" ];
+
+          postInstall = ''
+            mkdir -p $out/share/applications
+            cat > $out/share/applications/opentrack.desktop << EOF
+            [Desktop Entry]
+            Name=opentrack
+            GenericName=Head tracking software
+            Exec=opentrack
+            Icon=$out/share/pixmaps/opentrack.png
+            Terminal=false
+            Type=Application
+            Categories=Utility;
+            EOF
+
+            mkdir -p $out/share/pixmaps
+            cp $src/gui/images/opentrack.png $out/share/pixmaps/
+            wrapQtApp $out/bin/opentrack
+          '';
+
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/opentrack/opentrack";
+            description = "Head tracking software with ONNX support";
+            mainProgram = "opentrack";
+            changelog = "https://github.com/opentrack/opentrack/releases/tag/opentrack-${version}";
+            license = licenses.isc;
+            platforms = platforms.linux;
+          };
+        };
       in
       {
         packages = {
-          default = opentrackWithOnnx;
-          opentrack = opentrackWithOnnx;
-        };
-
-        apps = {
-          default = {
-            type = "app";
-            program = "${opentrackWithOnnx}/bin/opentrack";
-          };
+          default = opentrack;
+          opentrack = opentrack;
         };
       }
     );
