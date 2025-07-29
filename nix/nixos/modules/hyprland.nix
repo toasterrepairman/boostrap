7b673fba7d555706acb38b5b95d81f847bc3a165
Joel B. <smol@toast.cyou>
2025-07-28 21:10:13 -0400

    fresh faces

--- nix/nixos/modules/hyprland.nix
+++ nix/nixos/modules/hyprland.nix
@@ -4,6 +4,18 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
+    # stuff
+    kitty
+    waybar
+    nwg-look
+    xorg.xcursorthemes
+    xcursor-pro
+    rofi
+    cliphist
+    dunst
+    grim
+    slurp
+
     xwayland
     tuba
     cartridges
@@ -21,7 +33,19 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    xwayland.enable = true;
   };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
