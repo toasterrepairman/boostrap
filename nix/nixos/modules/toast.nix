12c67193dd8c633379277076bb1903bc149ef316
Joel B. <smol@toast.cyou>
2026-09-13 14:35:11 -0400

    remove override

--- nix/nixos/modules/toast.nix
+++ nix/nixos/modules/toast.nix
@@ -95,7 +95,6 @@ in {
   # delete these before reaching the gates of heaven
   environment.sessionVariables = rec {
     GOPATH = "\${HOME}/.go";
-    GTK_THEME = "adw-gtk3-dark";
   };
 
   # i trust these are unneccesary but i dont know anymore
